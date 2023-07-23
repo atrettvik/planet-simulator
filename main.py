21345def13a474f4e99695e9fb029a6a28e0cf6e
atrettvik <139919019+atrettvik@users.noreply.github.com>
2023-07-23 19:36:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ WHITE = (255, 255, 255)
 
 
 class Planet:
+    AU = 149.6e6 * 1000
+    G = 6.67427e-11
+    SCALE = 250 / AU  # 1AU = 100 pixels
+    TIMESTEP = 3600 * 24  # 1 day
+
     def __init__(self, x, y, radius, color, mass):
         self.x = x
         self.y = y
@@ -22,8 +27,16 @@ class Planet:
         self.color = color
         self.mass = mass
 
+        self.orbit = []
+        self.sun = False
+        self.distance_to_sun = 0
+
         self.x_vel = 0
         self.y_vel = 0
+
+    def draw(self, win):
+        x = self.x * self.SCALE
+        y = self.y * self.SCALE
 
 
 # EVENT LOOP
