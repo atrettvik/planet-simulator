67ea5695b9138ad9286e9cc4102d3cd0b1a4a4b6
atrettvik <139919019+atrettvik@users.noreply.github.com>
2023-07-23 19:59:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0)
 BLUE = (100, 149, 237)
 RED = (188, 39, 50)
+DARK_GREY = (80, 78, 81)
 
 # CLASS?
 
@@ -56,8 +57,10 @@ def main():
     sun.sun = True
     earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24)
     mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23)
+    mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GREY, 3.30 * 10**23)
+    venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10**24)
 
-    planets = [sun, earth, mars]
+    planets = [sun, earth, mars, mercury, venus]
 
     # MAIN LOOP
 
