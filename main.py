a68c15c1f5ac29065600c960767fcd1963b1f3dc
atrettvik <139919019+atrettvik@users.noreply.github.com>
2023-07-25 11:41:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -48,16 +48,33 @@ class Planet:
         other_x, other_y = other.x, other.y
         distance_x = other.x - self.x
         distance_y = other.y - self.y
-        distance = math.sqrt(distance_x**2 + distance.y**2)
+        distance = math.sqrt(distance_x**2 + distance_y**2)
 
         if other.sun:
             self.distance_to_sun = distance
 
-        force = self.g * self.mass * other.mass / distance**2
+        force = self.G * self.mass * other.mass / distance**2
         theta = math.atan2(distance_y, distance_x)
         force_x = math.cos(theta) * force
-        force_y = math.cos(theta) * force
+        force_y = math.sin(theta) * force
         return force_x, force_y
+
+    def update_position(self, planets):
+        total_fx = total_fy = 0
+        for planet in planets:
+            if self == planet:
+                continue
+
+            fx, fy = self.attraction(planet)
+            total_fx += fx
+            total_fy += fy
+
+        self.x_vel += total_fx / self.mass * self.TIMESTEP
+        self.y_vel += total_fx / self.mass * self.TIMESTEP
+
+        self.x = self.x_vel * self.TIMESTEP
+        self.y = self.y_vel * self.TIMESTEP
+        self.orbit.append((self.x, self.y))
 
 
 # EVENT LOOP
@@ -88,7 +105,9 @@ def main():
                 run = False
 
         for planet in planets:
+            planet.update_position(planets)
             planet.draw(WIN)
+
         pygame.display.update()
 
     pygame.quit()
